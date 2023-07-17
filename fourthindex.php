b26cc4f01370ebdd4df1f5ffe84fe989fbebf73f
Freek1ll3R <gugakobakhidze18@gmail.com>
2023-07-17 19:25:56 +0400

    Add Third section and start fourth

---
+++ fourthindex.php
@@ -0,0 +1,5 @@
+<?php include_once "./header.php" ?>
+<?php include "./assets/functions/variables.php" ?>
+<img class="BG_image3" src="assets/images/fourthpagebg.png" alt="BG">
+
+<?php include_once "./footer.php" ?>
--- thirdindex.php
+++ thirdindex.php
@@ -19,7 +19,6 @@
         </div>
     </section>
     <section class="third_section_page3">
-
         <div class="grid_content">
             <div class="thirdPage_card">
                 <div class="btn btn2">
@@ -38,7 +37,6 @@
             </div>
         </div>
         <img class="thirdpagecontent" src="assets/images/thirdpagecontent.png" alt="content">
-
     </section>
     <section class="main_section">
         <div class="main_section-content">
@@ -49,6 +47,110 @@
                 <h2>Analyze your Unique, <br>
                     Multidimensional data.</h2>
             </div>
+            <div class="data_cards">
+                <div class="data_card">
+                    <img src="assets/images/datacard1.png" alt="">
+                    <div class="data_card-img">
+                        <img src="assets/images/circle.png" alt="circle">
+                        <h4>Full Motion Video</h4>
+                    </div>
+                </div>
+                <div class="data_card">
+                    <img src="assets/images/datacard2.png" alt="">
+                    <div class="data_card-img">
+                        <img src="assets/images/circle.png" alt="circle">
+                        <h4>Geospatial Data</h4>
+                    </div>
+                </div>
+                <div class="data_card">
+                    <img src="assets/images/datacard3.png" alt="">
+                    <div class="data_card-img">
+                        <img src="assets/images/circle.png" alt="circle">
+                        <h4>Image Data</h4>
+                    </div>
+                </div>
+            </div>
+            <a class="between_classes" href="#">Learn More →</a>
+            <div class="section3_page3">
+                <div class="third_section3_page3">
+                    <img src="assets/images/inteligence.png" alt="inteligence">
+                    <div class="thirdPage_card">
+                        <div class="btn btn2">
+                            <a href="#">say goodbye to labeling</a>
+                        </div>
+                        <h2>The new standard <br>
+                            of Intelligence.</h2>
+                        <p>The days of labeling data by hand over weeks, months, or even <br>years are gone. LiDAR is an
+                            end-to-end AI pipeline that automates <br>the analysis of a variety of large, unstructured
+                            datasets,
+                            from <br>security camera footage to geospatial imagery. You can ingest, <br>search, and
+                            categorize your
+                            data - build and train models - run <br>inference - and integrate or access LiDAR through
+                            cloud-hosted <br>APIs. Integrate LiDAR into your current workflows today to
+                            <br>exponentially
+                            accelerate
+                            your speed-to-insight.
+                        </p>
+                    </div>
+                </div>
+                <a href="#">See More Examples →</a>
+            </div>
+            <div class="main_section-content partners_comp">
+                <div class="thirdPage_card for_partners">
+                    <div class="btn btn2 ">
+                        <a href="#">any kind of data</a>
+                    </div>
+                    <h2>Analyze your Unique, <br>
+                        Multidimensional data.</h2>
+                </div>
+                <div class="partners">
+                    <img src="assets/images/partner1.png" alt="partner">
+                    <img src="assets/images/partner2.png" alt="partner">
+                    <img src="assets/images/partner3.png" alt="partner">
+                    <img src="assets/images/partner4.png" alt="partner">
+                    <img src="assets/images/partner5.png" alt="partner">
+                </div>
+                <a href="#">View customere stories →</a>
+            </div>
+            <div class="third_section3_page3 third_section3_page4">
+                <div class="thirdPage_card">
+                    <div class="btn btn2">
+                        <a href="#">say goodbye to labeling</a>
+                    </div>
+                    <h2>AI in Minutes, <br>
+                        Not Months.</h2>
+                    <p>You don’t really want AI, you want answers. Getting them today <br>takes months - or years - and
+                        costs millions of dollars.
+                        <br>What if you could get around the problem of needing massive <br>amounts of human-labeled
+                        data?
+                        What
+                        if you could use AI to get <br>critical insights - even when data is unstructured or sparse?
+                        <br>Instead of needing months of human labeling to have some base <br>AI performance in a new
+                        situation,
+                        RAIC can get to work <br>immediately, radically reducing the latency between data <br>collection
+                        and
+                        actionable insights.
+                    </p>
+                </div>
+                <img src="assets/images/third_section3_page.png" alt="inteligence">
+            </div>
+            <a class="third_section3_link" href="#">Learn More →</a>
+    </section>
+    <section class="eighth_section">
+        <div class="container">
+            <div class="main_section-content ninth_content">
+                <div class="btn btn2">
+                    <img src="assets/images/btnclick.svg" alt="btn" />
+                    <a href="#">accelerate your speed-to-insight</a>
+                </div>
+                <h1 class="content_title">Reach Us</h1>
+                <p>What if you could develop and train AI models in minutes without labeled data? <br>Let us help you
+                    transcend data limitations and unleash the value of AI in your <br>business today.</p>
+                <div class="btn btn2 btn3">
+                    <a href="#">Let’s Talk →</a>
+                </div>
+            </div>
+        </div>
     </section>
 </main>
 <?php include "./footer.php" ?>
